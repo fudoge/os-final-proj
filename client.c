b70f0d8a912c6aaab7307ff1652297857e028a67
chaewoon <chaewoon@ubuntu20.04.myguest.virtualbox.org>
2025-06-14 23:41:08 +0900

    Fix:Client Bufsize Issue Resolved

--- client.c
+++ client.c
@@ -82,12 +82,12 @@ void *send_msg(void *arg) {
         fgets(buf, BUF_SIZE, stdin);
         if(strcmp(buf, "\n") == 0) continue;
         if(checkprefix(buf, "/setname")) {
-            char newname[NAME_MAX];
-            printf("Your new name is(max 31 byte(s)): ");
-            fgets(newname, BUF_SIZE, stdin);
+            char newname[NORMAL_SIZE-2];
+            printf("Your new name is(max 29 byte(s)): ");
+            fgets(newname, sizeof(newname), stdin);
             buf[strcspn(newname, "\n")] = 0;
 
-            char last_name[NAME_MAX];
+            char last_name[NORMAL_SIZE];
             snprintf(last_name, sizeof(last_name), "%s", name);
             snprintf(name, sizeof(name), "[%s]", newname);
             printf("Changed successfully\n");
